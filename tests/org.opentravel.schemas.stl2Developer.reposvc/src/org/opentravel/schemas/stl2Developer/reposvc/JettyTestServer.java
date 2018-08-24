0713bc0fe3ec6c29bcb588a09fe0e38b1d3b7adf
xmldave <dmh@mhxml.com>
2018-08-24 08:31:13 -0500

    Restored to previous version. Removed try/catch block so the exception could be caught in the BaseRepositoryTest class that introduces the error condition.

--- tests/org.opentravel.schemas.stl2Developer.reposvc/src/org/opentravel/schemas/stl2Developer/reposvc/JettyTestServer.java
+++ tests/org.opentravel.schemas.stl2Developer.reposvc/src/org/opentravel/schemas/stl2Developer/reposvc/JettyTestServer.java
@@ -36,228 +36,213 @@ import org.opentravel.schemacompiler.repository.RepositoryServlet;
 import org.opentravel.schemacompiler.repository.impl.RemoteRepositoryClient;
 
 /**
- * Encapsulates the configuration and run-time environment of an OTA2.0 repository that is launched
- * as an embedded Jetty application to facilitate testing.
+ * Encapsulates the configuration and run-time environment of an OTA2.0 repository that is launched as an embedded Jetty
+ * application to facilitate testing.
  * 
  * @author S. Livezey
  */
 public class JettyTestServer {
 
-    private Server jettyServer;
-    private final File repositorySnapshotLocation;
-    private final File repositoryRuntimeLocation;
-    private final File repositoryIndexLocation;
-    private final int port;
+	private Server jettyServer;
+	private final File repositorySnapshotLocation;
+	private final File repositoryRuntimeLocation;
+	private final File repositoryIndexLocation;
+	private final int port;
 
-    /**
-     * Constructor that specifies the configuration of the Jetty server to be used for testing.
-     * 
-     * @param port
-     *            the server port to which HTTP requests will be directed on the local host
-     * @param snapshotLocation
-     *            the folder location that contains an initial snapshot of the OTA2.0 repository
-     * @param ota2config
-     */
-    public JettyTestServer(int port, File snapshotLocation, File tmpLocation, File ota2config) {
-        this.repositorySnapshotLocation = snapshotLocation;
-        this.repositoryRuntimeLocation = new File(tmpLocation.getAbsolutePath(), "/test-repository");
-        this.repositoryIndexLocation = new File(repositoryRuntimeLocation.getParentFile(),
-                "/search-index");
-        this.port = port;
+	/**
+	 * Constructor that specifies the configuration of the Jetty server to be used for testing.
+	 * 
+	 * @param port
+	 *            the server port to which HTTP requests will be directed on the local host
+	 * @param snapshotLocation
+	 *            the folder location that contains an initial snapshot of the OTA2.0 repository
+	 * @param ota2config
+	 */
+	public JettyTestServer(int port, File snapshotLocation, File tmpLocation, File ota2config) {
+		this.repositorySnapshotLocation = snapshotLocation;
+		this.repositoryRuntimeLocation = new File(tmpLocation.getAbsolutePath(), "/test-repository");
+		this.repositoryIndexLocation = new File(repositoryRuntimeLocation.getParentFile(), "/search-index");
+		this.port = port;
 
-        if ((repositorySnapshotLocation != null) && !repositorySnapshotLocation.exists()) {
-            throw new IllegalArgumentException("Repository Snapshot Not Found: "
-                    + repositorySnapshotLocation.getAbsolutePath());
-        }
-        if (!repositoryRuntimeLocation.exists() && !repositoryRuntimeLocation.mkdirs()) {
-            throw new IllegalArgumentException("Unable to create run-rime repository folder: "
-                    + repositoryRuntimeLocation.getAbsolutePath());
-        }
-        System.setProperty("stl2Developer.test.repo.runtime-location",
-                repositoryRuntimeLocation.getAbsolutePath());
-        System.setProperty("stl2Developer.test.repo.search-index-location",
-                repositoryIndexLocation.getAbsolutePath());
+		if ((repositorySnapshotLocation != null) && !repositorySnapshotLocation.exists()) {
+			throw new IllegalArgumentException(
+					"Repository Snapshot Not Found: " + repositorySnapshotLocation.getAbsolutePath());
+		}
+		if (!repositoryRuntimeLocation.exists() && !repositoryRuntimeLocation.mkdirs()) {
+			throw new IllegalArgumentException(
+					"Unable to create run-rime repository folder: " + repositoryRuntimeLocation.getAbsolutePath());
+		}
+		System.setProperty("stl2Developer.test.repo.runtime-location", repositoryRuntimeLocation.getAbsolutePath());
+		System.setProperty("stl2Developer.test.repo.search-index-location", repositoryIndexLocation.getAbsolutePath());
 
-        System.setProperty("ota2.repository.config", ota2config.getAbsolutePath());
-        // disable logging becouse of invalid slf4j version
-        org.eclipse.jetty.util.log.Log.setLog(new NoLogging());
-    }
+		System.setProperty("ota2.repository.config", ota2config.getAbsolutePath());
+		// disable logging becouse of invalid slf4j version
+		org.eclipse.jetty.util.log.Log.setLog(new NoLogging());
+	}
 
-    /**
-     * Initializes the run-time repository from the snapshot and launches the Jetty server.
-     * 
-     * @throws Exception
-     *             thrown if the server cannot be started
-     */
-    public synchronized void start() throws Exception {
-        if (jettyServer != null) {
-            throw new IllegalStateException("The Jetty server is already running.");
-        }
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        
-        context.setContextPath("/ota2-repository-service");
-        context.addServlet(new ServletHolder(new RepositoryServlet(new RepositoryApplication())), "/service/*");
-        jettyServer = new Server(port);
-        jettyServer.setHandler(context);
-        initializeRuntimeRepository();
-        jettyServer.start();
+	/**
+	 * Initializes the run-time repository from the snapshot and launches the Jetty server.
+	 * 
+	 * @throws Exception
+	 *             thrown if the server cannot be started
+	 */
+	public synchronized void start() throws Exception {
+		if (jettyServer != null) {
+			throw new IllegalStateException("The Jetty server is already running.");
+		}
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
-        initializeRepositoryServices();
-        indexTestRepository();
-    }
+		context.setContextPath("/ota2-repository-service");
+		context.addServlet(new ServletHolder(new RepositoryServlet(new RepositoryApplication())), "/service/*");
+		jettyServer = new Server(port);
+		jettyServer.setHandler(context);
+		initializeRuntimeRepository();
+		jettyServer.start();
 
-    /**
-     * Adds this test server instance to the given repository manager.
-     * 
-     * @param manager
-     *            the repository manager instance to configure
-     * @throws RepositoryException
-     *             thrown if the configuration settings cannot be modified
-     */
-    public RemoteRepository configureRepositoryManager(RepositoryManager manager)
-            throws RepositoryException {
-        RemoteRepository testRepository = (RemoteRepository) manager
-                .getRepository("test-repository");
+		initializeRepositoryServices();
+		indexTestRepository();
+	}
 
-        if (testRepository == null) {
-            try {
-				testRepository = manager.addRemoteRepository("http://localhost:" + port
-				        + "/ota2-repository-service");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	/**
+	 * Adds this test server instance to the given repository manager.
+	 * 
+	 * @param manager
+	 *            the repository manager instance to configure
+	 * @throws RepositoryException
+	 *             thrown if the configuration settings cannot be modified
+	 */
+	public RemoteRepository configureRepositoryManager(RepositoryManager manager) throws RepositoryException {
+		RemoteRepository testRepository = (RemoteRepository) manager.getRepository("test-repository");
+
+		if (testRepository == null) {
+			testRepository = manager.addRemoteRepository("http://localhost:" + port + "/ota2-repository-service");
+		}
+		return testRepository;
+	}
+
+	/**
+	 * Shuts down the Jetty server.
+	 * 
+	 * @throws Exception
+	 *             thrown if the server cannot be shut down
+	 */
+	public synchronized void stop() throws Exception {
+		if (jettyServer == null) {
+			throw new IllegalStateException("The Jetty server is not running.");
+		}
+		jettyServer.stop();
+		jettyServer.join();
+		jettyServer = null;
+	}
+
+	/**
+	 * Pings the Jetty service with a meta-data request that forces the initialization of the repository web service.
+	 */
+	private void initializeRepositoryServices() throws Exception {
+		RemoteRepositoryClient.getRepositoryMetadata("http://localhost:" + port + "/ota2-repository-service");
+	}
+
+	/**
+	 * Indexes the contents of the server's test repository.
+	 */
+	private void indexTestRepository() throws Exception {
+		FreeTextSearchService service = FreeTextSearchServiceFactory.getInstance();
+
+		while (!service.isRunning()) {
+			try {
+				System.out.println("Waiting for Indexing Service startup...");
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
 			}
-        }
-        return testRepository;
-    }
+		}
+		service.indexAllRepositoryItems();
+	}
 
-    /**
-     * Shuts down the Jetty server.
-     * 
-     * @throws Exception
-     *             thrown if the server cannot be shut down
-     */
-    public synchronized void stop() throws Exception {
-        if (jettyServer == null) {
-            throw new IllegalStateException("The Jetty server is not running.");
-        }
-        jettyServer.stop();
-        jettyServer.join();
-        jettyServer = null;
-    }
+	/**
+	 * Initializes the run-time OTA2.0 repository by deleting any existing files and copying all of the files in the
+	 * snapshot folder location.
+	 * 
+	 * @throws IOException
+	 *             thrown if the contents of the repository cannot be initialized
+	 */
+	public void initializeRuntimeRepository() throws IOException {
+		RepositoryTestUtils.deleteContents(repositoryIndexLocation);
+		RepositoryTestUtils.deleteContents(repositoryRuntimeLocation);
 
-    /**
-     * Pings the Jetty service with a meta-data request that forces the initialization of the
-     * repository web service.
-     */
-    private void initializeRepositoryServices() throws Exception {
-        RemoteRepositoryClient.getRepositoryMetadata("http://localhost:" + port
-                + "/ota2-repository-service");
-    }
+		if (repositorySnapshotLocation != null) {
+			RepositoryTestUtils.copyContents(repositorySnapshotLocation, repositoryRuntimeLocation);
+		}
+	}
 
-    /**
-     * Indexes the contents of the server's test repository.
-     */
-    private void indexTestRepository() throws Exception {
-        FreeTextSearchService service = FreeTextSearchServiceFactory.getInstance();
+	public class RepositoryApplication extends ResourceConfig {
 
-        while (!service.isRunning()) {
-            try {
-                System.out.println("Waiting for Indexing Service startup...");
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-            }
-        }
-        service.indexAllRepositoryItems();
-    }
+		public RepositoryApplication() throws ClassNotFoundException {
+			registerClasses(RepositoryContentResource.class, JAXBContextResolver.class,
+					RepositoryServiceExceptionMapper.RepositoryExceptionMapper.class,
+					RepositoryServiceExceptionMapper.RepositorySecurityExceptionMapper.class,
+					RepositoryServiceExceptionMapper.JAXBExceptionMapper.class,
+					RepositoryServiceExceptionMapper.IOExceptionMapper.class, MultiPartFeature.class);
+		}
+	}
 
-    /**
-     * Initializes the run-time OTA2.0 repository by deleting any existing files and copying all of
-     * the files in the snapshot folder location.
-     * 
-     * @throws IOException
-     *             thrown if the contents of the repository cannot be initialized
-     */
-    public void initializeRuntimeRepository() throws IOException {
-        RepositoryTestUtils.deleteContents(repositoryIndexLocation);
-        RepositoryTestUtils.deleteContents(repositoryRuntimeLocation);
-
-        if (repositorySnapshotLocation != null) {
-            RepositoryTestUtils.copyContents(repositorySnapshotLocation, repositoryRuntimeLocation);
-        }
-    }
-    
-    public class RepositoryApplication extends ResourceConfig {
-    	
-    	public RepositoryApplication() throws ClassNotFoundException {
-    		registerClasses( RepositoryContentResource.class, JAXBContextResolver.class,
-    				RepositoryServiceExceptionMapper.RepositoryExceptionMapper.class,
-    				RepositoryServiceExceptionMapper.RepositorySecurityExceptionMapper.class,
-    				RepositoryServiceExceptionMapper.JAXBExceptionMapper.class,
-    				RepositoryServiceExceptionMapper.IOExceptionMapper.class,
-    				MultiPartFeature.class
-    			);
-    	}
-    }
-    
-    public class NoLogging implements Logger {
-        @Override
-        public String getName() {
-            return "no";
-        }
-
-        @Override
-        public void warn(String msg, Object... args) {
-        }
-
-        @Override
-        public void warn(Throwable thrown) {
-        }
-
-        @Override
-        public void warn(String msg, Throwable thrown) {
-        }
-
-        @Override
-        public void info(String msg, Object... args) {
-        }
-
-        @Override
-        public void info(Throwable thrown) {
-        }
-
-        @Override
-        public void info(String msg, Throwable thrown) {
-        }
-
-        @Override
-        public boolean isDebugEnabled() {
-            return false;
-        }
-
-        @Override
-        public void setDebugEnabled(boolean enabled) {
-        }
-
-        @Override
-        public void debug(String msg, Object... args) {
-        }
-
-        @Override
-        public void debug(Throwable thrown) {
-        }
+	public class NoLogging implements Logger {
+		@Override
+		public String getName() {
+			return "no";
+		}
 
 		@Override
-        public void debug(String msg, Throwable thrown) {
-        }
+		public void warn(String msg, Object... args) {
+		}
 
-        @Override
-        public Logger getLogger(String name) {
-            return this;
-        }
+		@Override
+		public void warn(Throwable thrown) {
+		}
 
-        @Override
-        public void ignore(Throwable ignored) {
-        }
-    }
+		@Override
+		public void warn(String msg, Throwable thrown) {
+		}
+
+		@Override
+		public void info(String msg, Object... args) {
+		}
+
+		@Override
+		public void info(Throwable thrown) {
+		}
+
+		@Override
+		public void info(String msg, Throwable thrown) {
+		}
+
+		@Override
+		public boolean isDebugEnabled() {
+			return false;
+		}
+
+		@Override
+		public void setDebugEnabled(boolean enabled) {
+		}
+
+		@Override
+		public void debug(String msg, Object... args) {
+		}
+
+		@Override
+		public void debug(Throwable thrown) {
+		}
+
+		@Override
+		public void debug(String msg, Throwable thrown) {
+		}
+
+		@Override
+		public Logger getLogger(String name) {
+			return this;
+		}
+
+		@Override
+		public void ignore(Throwable ignored) {
+		}
+	}
 }
